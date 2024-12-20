62eeb70221f15b0fbd00de0dfbfab6bfaa0d3612
Yamin Yamin <naifalhumaidi@proton.me>
2024-12-20 23:59:27 +0300

    Add some ui components (dialog,input and label) and setup prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { TodoDialog } from "@/components/TodoDialog";
 import Image from "next/image";
 
 export default function Home() {
   return <>
-    
+    <TodoDialog>
+      
+    </TodoDialog>
   </>;
 }
